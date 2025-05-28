e54f3de7809bdbf4f55fca1299fe8352243f50cb
Dante Osornio <cdmx2592@amerike.edu.mx>
2025-05-28 03:56:24 -0600

    Update supabaseClient.js

--- docs/js/supabaseClient.js
+++ docs/js/supabaseClient.js
@@ -11,7 +11,6 @@ module.exports = { supabase }
 
 // js/supabaseClient.js
 /*
-const { createClient } = supabase;
 const SUPABASE_URL = 'https://selrdrxvnhmoauacdoap.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNlbHJkcnh2bmhtb2F1YWNkb2FwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYzMDgwMjQsImV4cCI6MjA2MTg4NDAyNH0.ocgmf2Eli_losg8gBEinigt5sSfRGpF2aX7uEDX1Oa8';
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
@@ -20,6 +19,7 @@ const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 // ==============================
 // Configuración del cliente Supabase
 // ==============================
+const { createClient } = supabase;
 
 // 👉 Reemplaza con los valores reales de tu proyecto Supabase
 const SUPABASE_URL = 'https://selrdrxvnhmoauacdoap.supabase.co';
